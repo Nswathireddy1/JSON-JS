64aca3416c10b20fec646e47827f75680264e225
unknown <reddyswathi590@gmail.com>
2018-10-12 20:27:30 +0530

    first commit

---
+++ Person1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Web.Script.Serialization;
+using Newtonsoft.Json;
+
+namespace JavascriptSerialization
+{
+    class Program1
+    {
+        public string name { get; set; }
+        public int age { get; set; }
+        public override string ToString()
+        {
+            return string.Format("Name:{0}\n Age:{1}", name, age);
+        }
+    }
+    class Person1
+    {
+        static void Main(string[] args)
+        {
+            //deserialize json from file
+            String JSONstring = File.ReadAllText(@"F:\BIZRUNTIME\Practice\json-csharp\JavascriptSerialization\JavascriptSerialization\json1.json");
+            JavaScriptSerializer ser = new JavaScriptSerializer();
+            Program1 p1 = ser.Deserialize<Program1>(JSONstring);//JSONstring is an object
+            Console.WriteLine(p1);
+
+            Program p2 = new Program() { name = "Srrrrrrrr", age =23456789 };
+            string outputJSON = ser.Serialize(p2);
+            File.WriteAllText("ex.json", outputJSON);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Web.Script.Serialization;
+
+namespace JavascriptSerialization
+{
+    class Program
+    {
+          public string name { get; set; }
+          public int age { get; set; }
+        public override string ToString()
+        {
+            return string.Format("Name:{0}\n Age:{1}",name,age);
+        }
+    }
+    class Person
+    {
+        static void Main(string[] args)
+        {
+            //deserialize json from file
+            String JSONstring = File.ReadAllText(@"F:\BIZRUNTIME\Practice\json-csharp\JavascriptSerialization\JavascriptSerialization\json1.json");
+            JavaScriptSerializer ser = new JavaScriptSerializer();
+            Program p1 = ser.Deserialize<Program>(JSONstring);//JSONstring is an object
+            Console.WriteLine(p1);
+        }
+    }
+}
